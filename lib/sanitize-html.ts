58f95c01e456894b53ff47253c4ac6c0fb1dbc24
Salterramv <salterramv@outlook.com>
2026-08-30 18:06:53 +0000

    Allow rich education content in public renderer

--- lib/sanitize-html.ts
+++ lib/sanitize-html.ts
@@ -1,6 +1,7 @@
 import DOMPurify from "isomorphic-dompurify"
 
 const ALLOWED_TAGS = [
+  // Text
   "p",
   "br",
   "strong",
@@ -8,20 +9,70 @@ const ALLOWED_TAGS = [
   "em",
   "i",
   "u",
+  "s",
   "span",
+
+  // Headings
+  "h1",
   "h2",
   "h3",
   "h4",
+  "h5",
+  "h6",
+
+  // Lists
   "ul",
   "ol",
   "li",
+
+  // Links
   "a",
+
+  // Layout / editor content
+  "div",
+
+  // Tables
+  "table",
+  "thead",
+  "tbody",
+  "tfoot",
+  "tr",
+  "th",
+  "td",
+  "caption",
+
+  // Images
+  "img",
+
+  // Quotations
+  "blockquote",
+
+  // Other useful text elements
+  "hr",
+  "sup",
+  "sub",
 ]
 
 const ALLOWED_ATTR = [
+  // Links
   "href",
   "target",
   "rel",
+
+  // Images
+  "src",
+  "alt",
+  "title",
+  "width",
+  "height",
+
+  // Tables
+  "colspan",
+  "rowspan",
+  "scope",
+
+  // CURA editor formatting
+  "class",
   "style",
 ]
 
@@ -33,6 +84,7 @@ export function sanitizeRichText(
   return DOMPurify.sanitize(html, {
     ALLOWED_TAGS,
     ALLOWED_ATTR,
+
     FORBID_TAGS: [
       "script",
       "iframe",
@@ -44,16 +96,41 @@ export function sanitizeRichText(
       "textarea",
       "select",
       "style",
+      "base",
+      "meta",
+      "link",
     ],
+
     FORBID_ATTR: [
       "onerror",
       "onload",
       "onclick",
+      "ondblclick",
+      "onmousedown",
+      "onmouseup",
+      "onmousemove",
       "onmouseover",
+      "onmouseout",
+      "onmouseenter",
+      "onmouseleave",
+      "onkeydown",
+      "onkeyup",
+      "onkeypress",
       "onfocus",
       "onblur",
+      "onchange",
+      "onsubmit",
+      "oninput",
+      "onload",
       "srcdoc",
     ],
+
     ALLOW_DATA_ATTR: false,
+
+    /*
+     * Keep relative/internal URLs working while allowing
+     * normal HTTPS links and images.
+     */
+    ALLOW_UNKNOWN_PROTOCOLS: false,
   })
 }
